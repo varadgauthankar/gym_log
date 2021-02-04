591cfc1cb29d5b6824d2240507bb93a574a916c8
Varad Gauthankar <gvarad1601@gmail.com>
2021-02-04 20:56:09 +0530

    Create analytics.dart

---
+++ lib/pages/analytics.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:workout_tracker/utils/colors.dart';
+import 'package:workout_tracker/utils/textStyles.dart';
+
+class Analytics extends StatefulWidget {
+  @override
+  _AnalyticsState createState() => _AnalyticsState();
+}
+
+class _AnalyticsState extends State<Analytics> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyColors.white,
+        elevation: 3,
+        title: Text(
+          "Analytics",
+          style: AppBarTitleStyle.dark,
+        ),
+      ),
+      body: Text("helllllllllllllllllllllllllllllllllllo"),
+    );
+  }
+}
