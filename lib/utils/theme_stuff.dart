718d6da4efcb9a5a45b80ced70afe69a03e97d22
Varad Gauthankar <gvarad1601@gmail.com>
2021-05-26 00:44:07 +0530

    Delete theme_stuff.dart

--- lib/utils/theme_stuff.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:workout_tracker/utils/colors.dart';
-
-TextSelectionThemeData textSelectionTheme = TextSelectionThemeData(
-  cursorColor: MyColors.accentColor,
-  selectionHandleColor: MyColors.accentColor,
-);
