7c7b6eb79798c8f68ede3cba1829303cbabb7901
Varad Gauthankar <gvarad1601@gmail.com>
2021-05-29 23:06:34 +0530

    Create empty_page.dart

---
+++ lib/widgets/empty_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:workout_tracker/utils/helpers.dart';
+import 'package:workout_tracker/utils/textStyles.dart';
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/no_data.png',
+            height: 180.0,
+            width: 180.0,
+          ),
+          SizedBox(height: 14),
+          Text(
+            'No exercises yet!',
+            style:
+                isThemeDark(context) ? NoDataHeading.dark : NoDataHeading.light,
+          ),
+          Text(
+            'click  +  to add the exercise',
+            style: isThemeDark(context)
+                ? NoDataSubtitle.dark
+                : NoDataSubtitle.light,
+          ),
+        ],
+      ),
+    );
+  }
+}
